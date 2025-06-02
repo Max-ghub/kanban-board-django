e17f801edf4b55c33396c33ea4e27d270e606f6a
Max <max_artyukhin@mail.ru>
2025-06-02 18:10:00 +0500

    improvment users app

--- users/utils.py
+++ users/utils.py
@@ -5,7 +5,6 @@ from users.models import User
 
 def validation_new_user(phone, username):
     """Валидация нового пользователя"""
-
     if User.objects.filter(phone=phone).exists():
         raise ValidationError("Пользователей с таким номером телефона уже существует")
     if User.objects.filter(username=username).exists():
--- users/views/registration.py
+++ users/views/registration.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -15,45 +16,53 @@ User = get_user_model()
 @require_POST
 def registration_view(request):
     """Регистрация пользователя"""
-    data = parse_request_json_data(request)
+    try:
+        data = parse_request_json_data(request)
 
-    required = ["phone", "username", "password"]
-    validate_request_json_data(data, required)
+        required = ["phone", "username", "password"]
+        validate_request_json_data(data, required)
 
-    phone = data.pop("phone")
-    username = data.pop("username")
-    password = data.pop("password")
-    extra_fields = data
-    validation_new_user(phone, username)
+        phone = data.pop("phone")
+        username = data.pop("username")
+        password = data.pop("password")
+        extra_fields = data
+        validation_new_user(phone, username)
 
-    User.objects.create_user(
-        phone=phone, username=username, password=password, **extra_fields
-    )
+        User.objects.create_user(
+            phone=phone, username=username, password=password, **extra_fields
+        )
 
-    return JsonResponse(
-        {
-            "message": f"На телефон {phone} был отправлен код для подтверждения регистрации"
-        }
-    )
+        return JsonResponse(
+            {
+                "message": f"На телефон {phone} был отправлен код для подтверждения регистрации"
+            },
+            status=201,
+        )
+
+    except ValidationError as e:
+        return JsonResponse({"error": e.message}, status=400)
 
 
 @csrf_exempt
 @require_POST
 def confirm_registration_view(request):
     """Подтверждение регистрации пользователя"""
-    data = parse_request_json_data(request)
-    required = ["phone", "code"]
-    validate_request_json_data(data, required)
+    try:
+        data = parse_request_json_data(request)
+        required = ["phone", "code"]
+        validate_request_json_data(data, required)
 
-    phone = data["phone"]
-    code = data["code"]
+        phone = data["phone"]
+        code = data["code"]
 
-    PhoneCode.verify_phone_code(phone, code)
-    PhoneCode.delete_phone_codes(phone)
+        PhoneCode.verify_phone_code(phone, code)
+        PhoneCode.delete_phone_codes(phone)
 
-    user = User.objects.get(phone=phone)
-    user.is_active = True
-    user.save()
+        user = User.objects.get(phone=phone)
+        user.is_active = True
+        user.save()
 
-    token = generate_tokens(user)
-    return JsonResponse(token)
+        token = generate_tokens(user)
+        return JsonResponse(token)
+    except ValidationError as e:
+        return JsonResponse({"error": e.message}, status=400)
