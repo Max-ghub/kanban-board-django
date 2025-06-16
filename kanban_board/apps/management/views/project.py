855b2160a506fc5904f96e86fd3cff99b9c912ba
Maximilian <max_artyukhin@mail.ru>
2025-06-16 16:48:19 +0500

    refactor management project

--- kanban_board/apps/management/views/project.py
+++ kanban_board/apps/management/views/project.py
@@ -15,15 +15,10 @@ class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.prefetch_related("members").all()
     serializer_class = ProjectSerializer
 
-    @action(
-        detail=True,
-        methods=["post"],
-        url_path="members",
-        serializer_class=ProjectMemberSerializer,
-    )
+    @action(detail=True, methods=["post"], url_path="members")
     def add_member(self, request, pk=None):
         project = self.get_object()
-        serializer = self.get_serializer(data=request.data)
+        serializer = ProjectMemberSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         updated = ProjectMemberService.add_member(
@@ -32,15 +27,10 @@ class ProjectViewSet(ModelViewSet):
 
         return Response(ProjectSerializer(updated).data, status=status.HTTP_201_CREATED)
 
-    @action(
-        detail=True,
-        methods=["delete"],
-        url_path="members/(?P<user_id>[^/.]+)",
-        serializer_class=ProjectMemberSerializer,
-    )
+    @action(detail=True, methods=["delete"], url_path="members/(?P<user_id>[^/.]+)")
     def delete_member(self, request, pk=None, user_id=None):
         project = self.get_object()
-        serializer = self.get_serializer(data={"user_id": user_id})
+        serializer = ProjectMemberSerializer(data={"user_id": user_id})
         serializer.is_valid(raise_exception=True)
 
         updated = ProjectMemberService.delete_member(
