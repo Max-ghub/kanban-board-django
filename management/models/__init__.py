85ddde34a3f2689280b79065cf7340c4de03ffda
Maximilian <max_artyukhin@mail.ru>
2025-06-19 03:19:49 +0500

    improvment flake8 filter

--- management/models/__init__.py
+++ management/models/__init__.py
@@ -1,5 +1,5 @@
-from .board import Board  # noqa: F401
-from .column import Column  # noqa: F401
-from .project import Project  # noqa: F401
-from .relation_task import RelationTask, RelationTaskType  # noqa: F401
-from .task import Task  # noqa: F401
+from .board import Board
+from .column import Column
+from .project import Project
+from .relation_task import RelationTask, RelationTaskType
+from .task import Task
