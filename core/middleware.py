aef966e0a27afdee18740ad333e68e44c08017b9
Maximilian <max_artyukhin@mail.ru>
2025-07-04 13:24:34 +0300

    feat: add maintenance feature-flag

--- core/middleware.py
+++ core/middleware.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from extra_settings.models import Setting
+from rest_framework import status
 
 
 class MaintenanceModeMiddleware:
-    def __init__(self, get_response ):
+    def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         if Setting.get("MAINTENANCE_MODE", False):
-            return HttpResponse("Идут технические работы")
+            if not request.path.startswith("/admin/") or request.path.startswith(
+                "/static/"
+            ):
+                return HttpResponse(
+                    "Идут технические работы",
+                    status=status.HTTP_503_SERVICE_UNAVAILABLE,
+                )
         return self.get_response(request)
