9eb17394096631574891e89a93e9da30fc4383dd
Maximilian <max_artyukhin@mail.ru>
2025-07-04 13:20:49 +0300

    fix: notification_perferences signal

--- notification_preferences/signals.py
+++ notification_preferences/signals.py
@@ -1,9 +1,9 @@
 from django.dispatch import receiver
 
-from notification.models import NotificationSettings
+from notification_preferences.models import NotificationPreferences
 from users.signals import user_activated
 
 
 @receiver(user_activated)
-def create_notification_settings(sender, user, **kwargs):
-    NotificationSettings.objects.get_or_create(user=user)
+def create_notification_preferences_settings_on_user_activated(sender, user, **kwargs):
+    NotificationPreferences.objects.get_or_create(user=user)
