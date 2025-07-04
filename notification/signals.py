8b60078427b6b31f4670d85d5a7c7731df09d754
Maximilian <max_artyukhin@mail.ru>
2025-07-04 13:19:42 +0300

    refactor: rename notification app signal

--- notification/signals.py
+++ notification/signals.py
@@ -5,5 +5,5 @@ from users.signals import user_activated
 
 
 @receiver(user_activated)
-def create_notification_settings(sender, user, **kwargs):
+def create_notification_settings_on_user_activated(sender, user, **kwargs):
     NotificationSettings.objects.get_or_create(user=user)
