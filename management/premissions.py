0e685197e6b6fc47fa3885dd75315ae596f48a67
Maximilian <max_artyukhin@mail.ru>
2025-09-14 14:20:33 +0500

    feat: add kanban_board management permissions

---
+++ management/premissions.py
@@ -0,0 +1,39 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+def _resolve_project(obj):
+    if hasattr(obj, "owner"):
+        return obj
+    elif hasattr(obj, "project"):
+        return obj.project
+    elif hasattr(obj, "board"):
+        return obj.board.project
+    elif hasattr(obj, "column"):
+        return obj.column.board.project
+    else:
+        return None
+
+
+class IsProjectMemberOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        project = _resolve_project(obj)
+        if project is None:
+            return False
+
+        user = request.user
+        if project.owner == user:
+            return True
+        elif project.members.filter(pk=user.pk).exists():
+            return request.method in SAFE_METHODS
+
+        return False
+
+
+class IsProjectOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        project = _resolve_project(obj)
+        if project is None:
+            return False
+
+        user = request.user
+        return project.owner == user
