8eda43c6ef765d510721fec9feaefc4518482a18
Maximilian <max_artyukhin@mail.ru>
2025-10-02 14:04:24 +0500

    fix: celery DJANGO_SETTINGS_MODULE value

--- kanban_board/settings/celery.py
+++ kanban_board/settings/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kanban_board.settings.main")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kanban_board.settings.dev")
 app = Celery("kanban")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
